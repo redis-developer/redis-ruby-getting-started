2c05adcbf32504a613d135c617ca91136982f7a6
Ajeet Singh Raina, Docker Captain, RedisLabs <ajeetraina@gmail.com>
2021-03-02 18:14:57 +0530

    Update connect.rb

--- connect.rb
+++ connect.rb
@@ -1,4 +1,4 @@
 require 'redis'
-redis = Redis.new(host: "localhost", port: 6379, db: 11)
+redis = Redis.new(host: "localhost", port: 6379, db: 1)
 redis.set("mykey", "hello world")
 redis.get("mykey")
